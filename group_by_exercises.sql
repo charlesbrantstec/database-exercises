8d2ed1ba2f15c402fb1ec3b650653f5c5aab67e8
Charles Brant-Stec <charlesbrantstec@gmail.com>
2020-09-09 11:38:26 -0500

    alias exercise

---
+++ group_by_exercises.sql
@@ -0,0 +1,13 @@
+USE employees;
+
+SELECT DISTINCT title FROM titles;
+
+SELECT last_name FROM employees WHERE last_name LIKE 'E%E' GROUP BY first_name, last_name;
+
+SELECT last_name, COUNT(*) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name ORDER BY COUNT(*) DESC;
+
+SELECT COUNT(*), gender FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') GROUP BY gender;
+
+SELECT COUNT(*) AS '# of Occurences', gender FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') GROUP BY gender;
+
+
